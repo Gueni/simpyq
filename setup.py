342e15d6e3a7158af641d6dd4d50950670368bc9
Mohamed Gueni | محمد قاني <20975550+Gueni@users.noreply.github.com>
2025-05-08 17:38:32 +0100

    Update version to 1.0.1 and enhance long description in setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="simpyq",
-    version="1.0.0",
+    version="1.0.1",
     author="Mohamed Gueni",
     author_email="mohamedgueni@outlook.com",
     description="CLI tool to query and analyze simulation CSV data with natural language",
-    long_description="CLI tool to query and analyze simulation CSV data with natural language. It uses OpenAI's GPT-3.5-turbo model to process and analyze the data, providing a user-friendly interface for data exploration.",
+    long_description=open("pypimd.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/Gueni/simpyq",
     packages=find_packages(),
